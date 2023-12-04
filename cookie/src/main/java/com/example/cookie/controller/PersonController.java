bc0d802df75cf3eeb1798d106960aa38650a587b
chang <ckdtns5262@nate.com>
2023-12-04 18:06:46 +0900

    [1204] 템플릿 변경

--- cookie/src/main/java/com/example/cookie/controller/PersonController.java
+++ cookie/src/main/java/com/example/cookie/controller/PersonController.java
@@ -1,28 +1,20 @@
 package com.example.cookie.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
-@Slf4j
-@RequestMapping("place")
-public class PlaceController {
+@RequestMapping("person")
+public class PersonController {
 
-	@GetMapping("/register")
-	public String Register(Model model) {
-		
-		return "place/register";
-	}
-	
-	@GetMapping("/list")
+	@GetMapping("list")
 	public String List() {
 		
-		return "place/list";
+		return "person/list";
 	}
+	
 }
