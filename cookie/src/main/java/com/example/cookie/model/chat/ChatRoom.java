8af723a444ed0e55e72d9baed731bea1affbe8d3
chang <ckdtns5262@nate.com>
2023-12-27 16:48:34 +0900

    [1227] 채팅기능 50% 완성

--- cookie/src/main/java/com/example/cookie/model/chat/ChatRoom.java
+++ cookie/src/main/java/com/example/cookie/model/chat/ChatRoom.java
@@ -10,15 +10,12 @@ import lombok.Data;
 
 @Data
 public class ChatRoom {
-
-	private String roomId;
-	private String name;
-	// 방 한개마다 여러사용자들을 set형태로 가지고 있게
-	private Set<WebSocketSession> sessions = new HashSet<>();
-	
-	@Builder
-	public ChatRoom(String roomId, String name) {
-		this.roomId = roomId;
-		this.name = name;
-	}
+    private Long roomId;
+    private String name;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+    @Builder
+    public ChatRoom(Long roomId, String name) {
+        this.roomId = roomId;
+        this.name = name;
+    }
 }
